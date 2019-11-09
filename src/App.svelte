00020ee945de0958351f42c59bc6308fd3e3382a
Price Comstock <price.comstock@gmail.com>
2019-11-09 16:20:22 -0500

    added a branding banner

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,24 @@
   });
 </script>
 
+<!-- Logo color is #5D2E8C -->
+<section class="hero">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        <img src="logo.png" alt="Checklatey" width="400" />
+      </h1>
+      <h2 class="subtitle">
+        It's like
+        <a href="https://ninite.com/">Ninite</a>
+        but with a package manager! Select the programs you want, get the
+        <a href="https://chocolatey.org/">Chocolatey</a>
+        command to install them all at once.
+      </h2>
+    </div>
+  </div>
+</section>
+
 <section class="section">
   <AppSelector {appsByCategory} bind:selectedByCategory />
 </section>
