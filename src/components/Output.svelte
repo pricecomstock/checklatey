d711d358a367497ad590c3e00ec82a4e56381fc4
Price Comstock <price.comstock@gmail.com>
2019-11-16 13:01:46 -0500

    added install chocolatey option

--- src/components/Output.svelte
+++ src/components/Output.svelte
@@ -1,9 +1,15 @@
 <script>
   let preferPortable = false;
   let sayYes = true;
+  let alsoInstallChocolatey = false;
+
+  const chocolateyInstallCommand =
+    "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\n";
 
   export let allSelected = [];
-  $: chocolateyCommand = `choco install ${sayYes ? "-y" : ""} ${allSelected
+  $: chocolateyCommand = `${
+    alsoInstallChocolatey ? chocolateyInstallCommand : ""
+  }choco install ${sayYes ? "-y" : ""} ${allSelected
     .map(app => {
       return preferPortable ? app.chocoPortable : app.chocoPackage;
     })
@@ -15,12 +21,16 @@
 </script>
 
 <style>
-
+  pre {
+    background-color: white;
+  }
 </style>
 
 <div class="container">
   <h1 class="title">Chocolatey Command</h1>
-  <div class="box is-family-code">{chocolateyCommand}</div>
+  <div class="box">
+    <pre>{chocolateyCommand}</pre>
+  </div>
   <button
     class="button is-large is-primary is-outlined"
     on:click={copyToClipboard}>
@@ -29,12 +39,23 @@
   <hr />
   <h2 class="subtitle">Options</h2>
   <label class="checkbox">
+    <input type="checkbox" bind:checked={sayYes} />
+    Don't prompt for user input (yes to everything)
+  </label>
+  <br />
+  <label class="checkbox">
     <input type="checkbox" bind:checked={preferPortable} />
     Prefer portable installs
   </label>
   <br />
-  <label class="checkbox">
-    <input type="checkbox" bind:checked={sayYes} />
-    Don't prompt for user input (yes to everything)
+  <label class="checkbox has-text-danger">
+    <input type="checkbox" bind:checked={alsoInstallChocolatey} />
+    Also Install Chocolatey (this is bad practice!) (nothing bad that happens is
+    my fault!)
+    <a
+      href="https://chocolatey.org/docs/installation#install-with-powershellexe">
+      (you should read this!)
+    </a>
+    (yes, you are piping a script into an administrator PowerShell!)
   </label>
 </div>
