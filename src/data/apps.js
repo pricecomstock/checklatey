7823455f02e134de3cfdaac44a9962fa295f1eb7
Price Comstock <price.comstock@gmail.com>
2019-11-17 13:29:12 -0500

    first attempt at icons but I think I'm going to have an issue dynamically importing this data

--- src/data/apps.js
+++ src/data/apps.js
@@ -1,3 +1,5 @@
+const icons = require("./icons");
+
 const apps = [];
 
 // This also controls the order on the web page
@@ -18,11 +20,18 @@ const cat = {
   DEVTOOLS: "Dev Tools"
 };
 
-function addApp(name, category, chocoPackage, chocoPortable = chocoPackage) {
+function addApp(
+  name,
+  category,
+  iconName,
+  chocoPackage,
+  chocoPortable = chocoPackage
+) {
   if (chocoPortable === "") chocoPortable = chocoPackage; // blank string can skip portable arg
   apps.push({
     name,
     category,
+    icon: icons[iconName],
     chocoPackage,
     chocoPortable,
     portableAvailable: chocoPackage !== chocoPortable,
@@ -31,126 +40,144 @@ function addApp(name, category, chocoPackage, chocoPortable = chocoPackage) {
 }
 
 // Browsers
-addApp("Firefox", cat.BROWSERS, "firefox");
-addApp("Google Chrome", cat.BROWSERS, "googlechrome");
-addApp("Chromium", cat.BROWSERS, "chromium");
-addApp("Vivaldi", cat.BROWSERS, "vivaldi");
-addApp("Opera", cat.BROWSERS, "opera");
-addApp("Brave", cat.BROWSERS, "brave");
+addApp("Firefox", cat.BROWSERS, "mozillafirefox", "firefox");
+addApp("Google Chrome", cat.BROWSERS, "googlechrome", "googlechrome");
+addApp("Chromium", cat.BROWSERS, "googlechrome", "chromium");
+addApp("Vivaldi", cat.BROWSERS, "zeit", "vivaldi");
+addApp("Opera", cat.BROWSERS, "zeit", "opera");
+addApp("Brave", cat.BROWSERS, "zeit", "brave");
 
 // Messaging
-addApp("Discord", cat.MESSAGING, "discord.install", "discord");
-addApp("Slack", cat.MESSAGING, "slack");
-addApp("Skype", cat.MESSAGING, "skype");
-addApp("Thunderbird", cat.MESSAGING, "thunderbird");
+addApp("Discord", cat.MESSAGING, "zeit", "discord.install", "discord");
+addApp("Slack", cat.MESSAGING, "zeit", "slack");
+addApp("Skype", cat.MESSAGING, "zeit", "skype");
+addApp("Thunderbird", cat.MESSAGING, "zeit", "thunderbird");
 
 // Security
-addApp("Malwarebytes", cat.SECURITY, "malwarebytes");
-addApp("Avast Free Antivirus", cat.SECURITY, "avastfreeantivirus");
-addApp("Keepass", cat.SECURITY, "keepass");
-addApp("QtPass", cat.SECURITY, "qtpass");
-addApp("LastPass", cat.SECURITY, "lastpass");
+addApp("Malwarebytes", cat.SECURITY, "zeit", "malwarebytes");
+addApp("Avast Free Antivirus", cat.SECURITY, "zeit", "avastfreeantivirus");
+addApp("Keepass", cat.SECURITY, "zeit", "keepass");
+addApp("QtPass", cat.SECURITY, "zeit", "qtpass");
+addApp("LastPass", cat.SECURITY, "zeit", "lastpass");
 
 // Development Languages
-addApp("Python", cat.DEVLANGS, "python");
-addApp("Python2", cat.DEVLANGS, "python2");
-addApp("NodeJS", cat.DEVLANGS, "nodejs");
-addApp("NodeJS (LTS)", cat.DEVLANGS, "nodejs-lts");
-addApp("JDK8", cat.DEVLANGS, "jdk8");
-addApp("Ruby", cat.DEVLANGS, "ruby");
-addApp("PHP", cat.DEVLANGS, "php");
-addApp("Strawberry Perl", cat.DEVLANGS, "strawberryperl");
+addApp("Python", cat.DEVLANGS, "zeit", "python");
+addApp("Python2", cat.DEVLANGS, "zeit", "python2");
+addApp("NodeJS", cat.DEVLANGS, "zeit", "nodejs");
+addApp("NodeJS (LTS)", cat.DEVLANGS, "zeit", "nodejs-lts");
+addApp("JDK8", cat.DEVLANGS, "zeit", "jdk8");
+addApp("Ruby", cat.DEVLANGS, "zeit", "ruby");
+addApp("PHP", cat.DEVLANGS, "zeit", "php");
+addApp("Strawberry Perl", cat.DEVLANGS, "zeit", "strawberryperl");
 
 // Dev Tools
-addApp("PowerShell", cat.DEVTOOLS, "powershell");
-addApp("Visual Studio Code", cat.DEVTOOLS, "vscode");
+addApp("PowerShell", cat.DEVTOOLS, "zeit", "powershell");
+addApp("Visual Studio Code", cat.DEVTOOLS, "zeit", "vscode");
 addApp(
   "Notepad++",
   cat.DEVTOOLS,
+  "zeit",
   "notepadplusplus.install",
   "notepadplusplus.commandline"
 );
-addApp("Git", cat.DEVTOOLS, "git", "git.portable");
-addApp("AWS CLI", cat.DEVTOOLS, "awscli");
-addApp("Azure CLI", cat.DEVTOOLS, "azure-cli");
-addApp("Docker CLI", cat.DEVTOOLS, "docker-cli");
-addApp("docker-compose", cat.DEVTOOLS, "docker-compose");
-addApp("OpenSSH", cat.DEVTOOLS, "openssh");
-addApp("WinSCP", cat.DEVTOOLS, "winscp", "winscp.portable");
-addApp("FileZilla", cat.DEVTOOLS, "filezilla", "filezilla.commandline");
-addApp("PuTTY", cat.DEVTOOLS, "putty", "putty.portable");
-addApp("Eclipse", cat.DEVTOOLS, "eclipse");
-addApp("cURL", cat.DEVTOOLS, "curl");
-addApp("GNU Wget", cat.DEVTOOLS, "wget");
-addApp("VirtualBox", cat.DEVTOOLS, "virtualbox");
-addApp("Wireshark", cat.DEVTOOLS, "wireshark");
+addApp("Git", cat.DEVTOOLS, "zeit", "git", "git.portable");
+addApp("AWS CLI", cat.DEVTOOLS, "zeit", "awscli");
+addApp("Azure CLI", cat.DEVTOOLS, "zeit", "azure-cli");
+addApp("Docker CLI", cat.DEVTOOLS, "zeit", "docker-cli");
+addApp("docker-compose", cat.DEVTOOLS, "zeit", "docker-compose");
+addApp("OpenSSH", cat.DEVTOOLS, "zeit", "openssh");
+addApp("WinSCP", cat.DEVTOOLS, "zeit", "winscp", "winscp.portable");
+addApp("FileZilla", cat.DEVTOOLS, "zeit", "filezilla", "filezilla.commandline");
+addApp("PuTTY", cat.DEVTOOLS, "zeit", "putty", "putty.portable");
+addApp("Eclipse", cat.DEVTOOLS, "zeit", "eclipse");
+addApp("cURL", cat.DEVTOOLS, "zeit", "curl");
+addApp("GNU Wget", cat.DEVTOOLS, "zeit", "wget");
+addApp("VirtualBox", cat.DEVTOOLS, "zeit", "virtualbox");
+addApp("Wireshark", cat.DEVTOOLS, "zeit", "wireshark");
 
 // Media
-addApp("Spotify", cat.MEDIA, "spotify");
-addApp("iTunes", cat.MEDIA, "itunes");
-addApp("VLC", cat.MEDIA, "vlc", "vlc.portable");
-addApp("foobar2000", cat.MEDIA, "foobar2000");
-addApp("MPC-HC", cat.MEDIA, "mpc-hc");
-addApp("K-Lite Codec Pack (Full)", cat.MEDIA, "k-litecodecpackfull");
-addApp("Handbrake", cat.MEDIA, "handbrake", "handbrake.portable");
+addApp("Spotify", cat.MEDIA, "zeit", "spotify");
+addApp("iTunes", cat.MEDIA, "zeit", "itunes");
+addApp("VLC", cat.MEDIA, "zeit", "vlc", "vlc.portable");
+addApp("foobar2000", cat.MEDIA, "zeit", "foobar2000");
+addApp("MPC-HC", cat.MEDIA, "zeit", "mpc-hc");
+addApp("K-Lite Codec Pack (Full)", cat.MEDIA, "zeit", "k-litecodecpackfull");
+addApp("Handbrake", cat.MEDIA, "zeit", "handbrake", "handbrake.portable");
 
 // Utility
-addApp("Everything", cat.UTILITIES, "everything", "everything.portable");
-addApp("TeraCopy", cat.UTILITIES, "teracopy");
-addApp("TreeSize Free", cat.UTILITIES, "treesizefree", "treesizefree.portable");
-addApp("CPU-Z", cat.UTILITIES, "cpu-z.install", "cpu-z.portable");
-addApp("GPU-Z", cat.UTILITIES, "gpu-z.portable");
-addApp("ImgBurn", cat.UTILITIES, "imgburn");
-addApp("Rufus", cat.UTILITIES, "rufus");
-addApp("Greenshot", cat.UTILITIES, "greenshot");
-addApp("AutoHotkey", cat.UTILITIES, "autohotkey");
-addApp("Sysinternals", cat.UTILITIES, "sysinternals");
-addApp("CCleaner", cat.UTILITIES, "ccleaner");
-addApp("Launchy", cat.UTILITIES, "launchy");
+addApp(
+  "Everything",
+  cat.UTILITIES,
+  "zeit",
+  "everything",
+  "everything.portable"
+);
+addApp("TeraCopy", cat.UTILITIES, "zeit", "teracopy");
+addApp(
+  "TreeSize Free",
+  cat.UTILITIES,
+  "zeit",
+  "treesizefree",
+  "treesizefree.portable"
+);
+addApp("CPU-Z", cat.UTILITIES, "zeit", "cpu-z.install", "cpu-z.portable");
+addApp("GPU-Z", cat.UTILITIES, "zeit", "gpu-z.portable");
+addApp("ImgBurn", cat.UTILITIES, "zeit", "imgburn");
+addApp("Rufus", cat.UTILITIES, "zeit", "rufus");
+addApp("Greenshot", cat.UTILITIES, "zeit", "greenshot");
+addApp("AutoHotkey", cat.UTILITIES, "zeit", "autohotkey");
+addApp("Sysinternals", cat.UTILITIES, "zeit", "sysinternals");
+addApp("CCleaner", cat.UTILITIES, "zeit", "ccleaner");
+addApp("Launchy", cat.UTILITIES, "zeit", "launchy");
 
 // File Sharing
-addApp("qBittorrent", cat.FILESHARING, "qbittorrent");
-addApp("Transmission", cat.FILESHARING, "transmission");
-addApp("Deluge", cat.FILESHARING, "deluge");
+addApp("qBittorrent", cat.FILESHARING, "zeit", "qbittorrent");
+addApp("Transmission", cat.FILESHARING, "zeit", "transmission");
+addApp("Deluge", cat.FILESHARING, "zeit", "deluge");
 
 // Creative
-addApp("Audacity", cat.CREATIVE, "audacity");
-addApp("LAME for Audacity", cat.CREATIVE, "audacity-lame");
-addApp("Unity Hub", cat.CREATIVE, "unity-hub");
-addApp("Blender", cat.CREATIVE, "blender");
-addApp("Paint.NET", cat.CREATIVE, "paint.net");
-addApp("GIMP", cat.CREATIVE, "gimp");
-addApp("Krita", cat.CREATIVE, "krita");
-addApp("Inkscape", cat.CREATIVE, "inkscape");
+addApp("Audacity", cat.CREATIVE, "zeit", "audacity");
+addApp("LAME for Audacity", cat.CREATIVE, "zeit", "audacity-lame");
+addApp("Unity Hub", cat.CREATIVE, "zeit", "unity-hub");
+addApp("Blender", cat.CREATIVE, "zeit", "blender");
+addApp("Paint.NET", cat.CREATIVE, "zeit", "paint.net");
+addApp("GIMP", cat.CREATIVE, "zeit", "gimp");
+addApp("Krita", cat.CREATIVE, "zeit", "krita");
+addApp("Inkscape", cat.CREATIVE, "zeit", "inkscape");
 
 // Compression
-addApp("7-Zip", cat.COMPRESSION, "7zip", "7zip.portable");
-addApp("PeaZip", cat.COMPRESSION, "peazip.install", "peazip");
-addApp("WinRAR", cat.COMPRESSION, "winrar");
+addApp("7-Zip", cat.COMPRESSION, "zeit", "7zip", "7zip.portable");
+addApp("PeaZip", cat.COMPRESSION, "zeit", "peazip.install", "peazip");
+addApp("WinRAR", cat.COMPRESSION, "zeit", "winrar");
 
 // Runtimes
-addApp("Microsoft Visual C++ Redistributable", cat.RUNTIMES, "vcredist140");
-addApp("Microsoft Silverlight", cat.RUNTIMES, "silverlight");
-addApp("Adobe Air", cat.RUNTIMES, "adobeair");
-addApp("Java Runtime (JRE) 8", cat.RUNTIMES, "javaruntime");
-addApp("Flash Player Plugin", cat.RUNTIMES, "flashplayerplugin");
+addApp(
+  "Microsoft Visual C++ Redistributable",
+  cat.RUNTIMES,
+  "zeit",
+  "vcredist140"
+);
+addApp("Microsoft Silverlight", cat.RUNTIMES, "zeit", "silverlight");
+addApp("Adobe Air", cat.RUNTIMES, "zeit", "adobeair");
+addApp("Java Runtime (JRE) 8", cat.RUNTIMES, "zeit", "javaruntime");
+addApp("Flash Player Plugin", cat.RUNTIMES, "zeit", "flashplayerplugin");
 
 // Cloud
-addApp("Dropbox", cat.CLOUD, "dropbox");
-addApp("Google Drive", cat.CLOUD, "googledrive");
-addApp("Nextcloud", cat.CLOUD, "nextcloud-client");
+addApp("Dropbox", cat.CLOUD, "zeit", "dropbox");
+addApp("Google Drive", cat.CLOUD, "zeit", "googledrive");
+addApp("Nextcloud", cat.CLOUD, "zeit", "nextcloud-client");
 
 // Documents
-addApp("Foxit PDF Reader", cat.DOCUMENTS, "foxitreader");
-addApp("Sumatra PDF Reader", cat.DOCUMENTS, "sumatrapdf");
-addApp("Adobe Reader", cat.DOCUMENTS, "adobe");
-addApp("PDFCreator", cat.DOCUMENTS, "pdfcreator");
-addApp("CutePDF", cat.DOCUMENTS, "cutepdf");
-addApp("LibreOffice", cat.DOCUMENTS, "libreoffice-fresh");
+addApp("Foxit PDF Reader", cat.DOCUMENTS, "zeit", "foxitreader");
+addApp("Sumatra PDF Reader", cat.DOCUMENTS, "zeit", "sumatrapdf");
+addApp("Adobe Reader", cat.DOCUMENTS, "zeit", "adobe");
+addApp("PDFCreator", cat.DOCUMENTS, "zeit", "pdfcreator");
+addApp("CutePDF", cat.DOCUMENTS, "zeit", "cutepdf");
+addApp("LibreOffice", cat.DOCUMENTS, "zeit", "libreoffice-fresh");
 
 // Gaming
-addApp("Steam", cat.GAMING, "steam");
-addApp("Epic Games Launcher", cat.GAMING, "epicgameslauncher");
+addApp("Steam", cat.GAMING, "zeit", "steam");
+addApp("Epic Games Launcher", cat.GAMING, "zeit", "epicgameslauncher");
 
 // console.table(apps);
 
